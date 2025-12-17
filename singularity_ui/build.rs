d862adc795f46fec4307579faab569327e091f9f
NYBACHOK <nybachokua@gmail.com>
2025-12-17 23:14:56 +0200

    material theme

--- singularity_ui/build.rs
+++ singularity_ui/build.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let config: slint_build::CompilerConfiguration =
-        slint_build::CompilerConfiguration::new().with_style("cosmic".into());
+    let config = slint_build::CompilerConfiguration::new()
+        .with_library_paths(std::collections::HashMap::from([(
+            "material".to_string(),
+            std::path::Path::new(&std::env::var_os("CARGO_MANIFEST_DIR").unwrap())
+                .join("themes/material-1.0/material.slint"),
+        )]))
+        .with_style("material".into());
 
     slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
 }
