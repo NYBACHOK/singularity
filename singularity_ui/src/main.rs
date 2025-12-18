e2c75db881357aab879d320986b1fa6cfa75569b
NYBACHOK <nybachok@proton.me>
2025-12-18 17:39:41 +0200

    messages addition

--- singularity_ui/src/main.rs
+++ singularity_ui/src/main.rs
@@ -1,6 +1,10 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::rc::Rc;
+
+use slint::{ModelRc, VecModel};
+
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,7 +12,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let ui = MainWindow::new()?;
 
-    ui.on_send_clicked(|_this| {});
+    let messages = Rc::new(VecModel::from(vec![ChatMessage {
+        text: "Hello! How can I help you?".into(),
+        is_user: false,
+    }]));
+
+    let messages_rc: ModelRc<ChatMessage> = messages.clone().into();
+    ui.set_messages(messages_rc);
+
+    ui.on_send_clicked({
+        let messages = messages.clone();
+        move |text| {
+            messages.push(ChatMessage {
+                is_user: true,
+                text,
+            });
+        }
+    });
 
     ui.run()?;
 
